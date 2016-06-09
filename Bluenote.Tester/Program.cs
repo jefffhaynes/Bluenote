2209fb9956a5ddb9ca05d84f613d3e54d9865e73
jefffhaynes <feedyurhed@gmail.com>
2016-06-09 16:53:45 -0400

    initial commit

---
+++ Bluenote.Tester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BluetoothGATTInterop;
+
+namespace BluetoothGATT.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var devices = BluetoothDeviceManager.GetDevices();
+
+            foreach(var device in devices)
+                Console.WriteLine(device);
+
+            Console.ReadKey();
+        }
+    }
+}
